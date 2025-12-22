374cab04225ef9f761633f24ef403465c971ad66
Brancen Gregory <brancengregory@gmail.com>
2025-12-22 01:07:33 -0600

    feat: Implement an is_prime checker with json parsing and robust error handling

---
+++ prime/src/main.rs
@@ -0,0 +1,126 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream};
+use std::io::{BufRead, Write, BufReader, BufWriter, Error, ErrorKind};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PrimeRequest {
+    method: String,
+    number: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PrimeResponse {
+    method: String,
+    prime: bool,
+}
+
+impl PrimeResponse {
+    fn new(req: &PrimeRequest) -> Self {
+        PrimeResponse {
+            method: "isPrime".to_string(),
+            prime: is_prime(req.number),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MalformedResponse {
+	method: String
+}
+
+impl MalformedResponse {
+	fn new() -> Self {
+		MalformedResponse{
+			method: "Malformed".to_string()
+		}
+	}
+
+	fn write(self, writer: &mut BufWriter<TcpStream>) -> std::io::Result<()> {
+		writer.write_all(&serde_json::to_vec(&self).expect("Couldn't serialize to JSON"))?;
+		writer.write_all(b"\n")?;
+		writer.flush()?;
+
+		Ok(())
+	}
+}
+
+fn is_prime(n: f64) -> bool {
+		if n < 0.0 || n.fract() != 0.0 {
+			return false
+		}
+
+		let num = n as u64;
+
+    match num {
+        0 | 1 => false,
+        2 => true,
+        _ if num % 2 == 0 => false,
+        _ => {
+            let limit = num.isqrt() + 1;
+            !(3..=limit).step_by(2).any(|i| num % i == 0)
+        }
+    }
+}
+
+fn handle_prime_request(request_str: &str, writer: &mut BufWriter<TcpStream>) -> std::io::Result<()> {
+    let req: PrimeRequest = serde_json::from_str(request_str)?;
+    println!("{:?}", req);
+
+		if req.method != "isPrime".to_string() {
+			return Err(Error::new(ErrorKind::InvalidData, "Invalid method"));
+		}
+
+    let resp = PrimeResponse::new(&req);
+    writer.write_all(&serde_json::to_vec(&resp).expect("Couldn't serialize JSON to bytes")).expect("Couldn't write response to buffer");
+
+		writer.write_all(b"\n").expect("Couldn't write newline to writer");
+		writer.flush().expect("Couldn't flush writer");
+    Ok(())
+}
+
+fn handle_client(stream: TcpStream) {
+    let write_stream = stream.try_clone().expect("Couldn't clone stream for writing");
+
+    let mut reader = BufReader::new(stream);
+    let mut writer = BufWriter::new(write_stream);
+
+    let mut line = String::new();
+    loop {
+        line.clear();
+        match reader.read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => {
+                if let Err(e) = handle_prime_request(&line, &mut writer) {
+										eprintln!("Failed to handle request: {}", e);
+										let resp = MalformedResponse::new();
+										if let Err(e) = resp.write(&mut writer) {
+											eprintln!("Failed to send malformed response: {}", e);
+										};
+										break;
+								}
+            },
+            Err(_) => break,
+
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+	let listener = TcpListener::bind("0.0.0.0:8080")?;
+
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				thread::spawn(move || {
+					handle_client(stream);
+				});
+			}
+			Err(e) => {
+				eprintln!("Connection failed: {}", e);
+			}
+		}
+	}
+
+	Ok(())
+}
